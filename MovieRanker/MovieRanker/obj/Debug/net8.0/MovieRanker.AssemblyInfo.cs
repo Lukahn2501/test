bca445fd3c39468da0f6c9be48678bcef13673f7
Kevin Roy <kevin.roy.ic@gmail.com>
2024-10-12 13:29:45 +0200

    .gitignore

--- MovieRanker/MovieRanker/obj/Debug/net8.0/MovieRanker.AssemblyInfo.cs
+++ MovieRanker/MovieRanker/obj/Debug/net8.0/MovieRanker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieRanker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4c2d293d70162b0dd2dd30d1f1f4ac588e44515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf90720e64c554578b4d15e310d57bd260b34c81")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieRanker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieRanker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
